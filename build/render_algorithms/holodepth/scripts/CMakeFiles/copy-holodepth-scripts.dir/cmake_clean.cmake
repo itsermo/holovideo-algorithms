d3322f3aa3d1b91d8c0fa3ec17de8ff942f9643d
OBMG <holo@media>
2014-07-26 16:33:50 -0400

    added build ignores, made the cmake script for holosuite better

--- build/render_algorithms/holodepth/scripts/CMakeFiles/copy-holodepth-scripts.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/copy-holodepth-scripts"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/copy-holodepth-scripts.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/render_algorithms/ripgen-fbo/scripts/CMakeFiles/copy-ripgen-scripts.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/copy-ripgen-scripts"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/copy-ripgen-scripts.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
